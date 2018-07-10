bc4cf101666f8067f58c4c4f6272f6a95b90f43c
Gary Hodgson <garethhodgson123@gmail.com>
2018-07-10 21:57:21 +0900

    Vuex stocks component

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,26 +1,24 @@
 <template>
+<div>
     <div class="row">
         <app-stock v-for="(stock, index) in stocks" :key="index" :stock="stock"></app-stock>
+    </div>
     </div>
 </template>
 
 
 <script>
 import Stock from './Stock.vue';
+import { mapGetters, mapMutations, mapActions} from "vuex";
+
 export default {
-    data() {
-        return {
-            stocks: [
-                { id:1, name: 'BMW', price: 110 },
-                { id:2, name: 'Google', price: 220 },
-                { id:3, name: 'Apple', price: 400 },
-                { id:4, name: 'Twitter', price: 900 },
-            ]
-        }
-    },
     components: {
         appStock: Stock
+    },
+    computed: {
+        stocks() {
+            return this.$store.getters.stocks;
+        }
     }
-    
-}
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import { routes } from './routes.js'; //curly as it is a named export
+//if i had of said export default i could have just done import store from './store/store'
+//export const use {} to import
+import { store } from './store/store.js';
 
 Vue.use(VueRouter);
 
@@ -13,5 +16,6 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 });
