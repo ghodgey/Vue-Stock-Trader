6367f02f19720b06fe2f74df61364e60087d8b2d
Gary Hodgson <garethhodgson123@gmail.com>
2018-07-12 21:58:30 +0900

    Stock End day randomization and dropdown toggler

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,12 +6,12 @@
         <router-link to="/portfolio" tag="li" class="nav-item" activeClass="active" exact><a class="nav-link">Portfolio</a></router-link>
     </ul>
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item nav-link">End Day</li>
+        <li class="nav-item nav-link" @click="endDay">End Day</li>
         <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" @click="isDropdownOpen = !isDropdownOpen" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Save & Load
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <div class="dropdown-menu" :class="{show: isDropdownOpen}" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#">Save Data</a>
           <a class="dropdown-item" href="#">Load Data</a>
         </div>
@@ -23,10 +23,23 @@
 </template>
 
  <script>
+import {mapActions} from 'vuex';
+
  export default {
+     data() {
+         return {
+             isDropdownOpen: false
+         }
+     },
      computed: {
          funds() {
              return this.$store.getters.funds;
+         }
+     },
+     methods: {
+         ...mapActions(['randomizeStocks']),
+         endDay() {
+             this.randomizeStocks();
          }
      }
  }
